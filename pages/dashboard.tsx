9b5685a97ef7944b3b7201ba5061d0f53be1ba60
SergeWilfried <63460822+SergeWilfried@users.noreply.github.com>
2023-06-25 13:36:13 +0000

    tx

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -15,7 +15,7 @@ export default function Dashboard({ rooms }: { rooms: Room[] }) {
   return (
     <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Head>
-        <title>RoomGPT Dashboard</title>
+        <title>Acceuil</title>
       </Head>
       <Header
         photo={session?.user?.image || undefined}
@@ -23,22 +23,21 @@ export default function Dashboard({ rooms }: { rooms: Room[] }) {
       />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mb-0 mb-8">
         <h1 className="mx-auto max-w-4xl font-display text-4xl font-bold tracking-normal text-slate-100 sm:text-6xl mb-5">
-          View your <span className="text-blue-600">room</span> generations
-        </h1>
+        Visualisez les générations de votre <span className="text-blue-600">pièce</span>.
+                </h1>
         {rooms.length === 0 ? (
           <p className="text-gray-300">
-            You have no room generations. Generate one{" "}
-            <Link
+            Vous n'avez pas de génération de pièces. Générer un{" "}            <Link
               href="/dream"
               className="text-blue-600 underline underline-offset-2"
             >
-              here
+              ici
             </Link>
           </p>
         ) : (
           <p className="text-gray-300">
-            Browse through your previous room generations below. Any feedback?
-            Email hassan@roomgpt.io
+            Parcourez les générations de piéces que vous avez créées ci-dessous. Vous avez des commentaires ?
+            Envoyez un courriel à hassan@roomgpt.io
           </p>
         )}
         {rooms.map((room) => (
