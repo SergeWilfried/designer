3f5d61735f99f085b142077f1234d80fd182c4ef
SergeWilfried <63460822+SergeWilfried@users.noreply.github.com>
2023-06-25 15:27:00 +0000

    add form submission

--- pages/dream.tsx
+++ pages/dream.tsx
@@ -311,7 +311,7 @@ const Home: NextPage = () => {
                     <input type="email" id="helper-text" aria-describedby="helper-text-explanation" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="gnadou@gmail.com" />
                     <p id="helper-text-explanation" className="mt-2 text-sm text-gray-500 dark:text-gray-400">Nous ne communiquerons jamais vos informations. Lire notre <a href="#" className="font-medium text-blue-600 hover:underline dark:text-blue-500">Politique de confidentialité</a>.</p>
                     <button
-                      onClick={() => signIn("email")}
+                      type="submit"
                       className="bg-gray-200 text-black font-semibold py-3 px-6 rounded-2xl flex items-center space-x-2"
                     >
                     
