aa4fa49d4813ce290f75b9607a0636e0b4b14160
SergeWilfried <63460822+SergeWilfried@users.noreply.github.com>
2023-06-25 14:32:24 +0000

    fix typo

--- pages/dream.tsx
+++ pages/dream.tsx
@@ -140,7 +140,7 @@ const Home: NextPage = () => {
   };
 
 
-  function handleChange(event: ChangeEvent<HTMLInputElement>): void {
+  function handleChange(event: any): void {
     throw new Error("Function not implemented.");
   }
 
