129d0b9b68796c5e103481c7ad84f9ad2954fadf
SergeWilfried <63460822+SergeWilfried@users.noreply.github.com>
2023-06-25 14:59:53 +0000

    fix auth

--- pages/dream.tsx
+++ pages/dream.tsx
@@ -280,13 +280,26 @@ const Home: NextPage = () => {
                       créer l'interieur de rêve pour votre piéce dès aujourd'hui. Vous obtiendrez 3 générations
                       gratuitement.
                     </div>
+                    <div className="mt-6">
+                    <form
+                    className="flex flex-col w-full space-y-4"
+                    onSubmit={handleSubmit(handleSubmitEmail)}
+                  >
+
+                    <label for="helper-text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Votre email</label>
+                    <input type="email" id="helper-text" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com">
+                    <p id="helper-text-explanation" class="mt-2 text-sm text-gray-500 dark:text-gray-400">We’ll never share your details. Read our <a href="#" class="font-medium text-blue-600 hover:underline dark:text-blue-500">Privacy Policy</a>.</p>
                     <button
-                      onClick={() => signIn("email")}
-                      className="bg-gray-200 text-black font-semibold py-3 px-6 rounded-2xl flex items-center space-x-2"
+                      laonClick={() => signIn("email")}
+                      cssName="bg-gray-200 text-black font-semibold py-3 px-6 rounded-2xl flex items-center space-x-2"
                     >
                     
                       <span>Se connecter</span>
                     </button>
+                    </form>
+
+                                      </div>
+                  
                   </div>
                 )
               )}
